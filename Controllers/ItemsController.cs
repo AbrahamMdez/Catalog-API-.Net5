e56571b497d0101eb2a20d836246148f7a246ec9
Abraham <nencho_1@hotmail.com>
2021-08-19 09:22:15 +0200

    created Dto and interface repository

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Dtos;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select( item => new ItemDto {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                CreatedDate = item.CreatedDate
+            });
+
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
